fd8b81f8f024ef1f178b3388e1aa20ea614d90a2
kanghwawon <corn1128@naver.com>
2023-03-20 21:58:12 +0900

    update 숙소예약

--- lib/data/index.ts
+++ lib/data/index.ts
@@ -1,5 +1,7 @@
 import user from "./user";
 import room from "./room";
-const Data = { user, room };
+import reservation from "./reservation";
+
+const Data = { user, room, reservation };
 
 export default Data;
